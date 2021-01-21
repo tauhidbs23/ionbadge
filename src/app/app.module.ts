470b9b14f74efeeb6e5815b950cc4dd6b21b2ddb
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-01-21 13:01:48 +0600

    ionic badge plugin not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Badge } from "@ionic-native/badge/ngx";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Badge
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Badge } from "@ionic-native/badge/ngx";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private badge: Badge) { }
+
+  set10Noti() {
+    console.log('14 no line click ');
+
+    setTimeout(()=>{
+      this.badge.hasPermission().then((res) => {
+        console.log('permission res', res);
+        // Success!
+        this.badge.set(10).then((res) => {
+            // Success!
+            // alert("set badge");
+            console.log('set badge res ',res);
+        }).catch((ex) => {
+          console.log('',JSON.stringify(ex));
+            // alert(JSON.stringify(ex));
+        });
+    }).catch((ex) => {
+      console.log('error ex ',JSON.stringify(ex));
+        // alert(JSON.stringify(ex));
+    });
+    
+
+    },5000);
+  }
+
+  ngOnInit(){
+  }
 
 }
